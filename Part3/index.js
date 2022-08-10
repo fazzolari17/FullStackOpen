0786c7d0106a28940ee58c4e8682d350438b8261
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-10 12:00:27 -0400

    initial commit

---
+++ Part3/index.js
@@ -0,0 +1,117 @@
+const { response } = require('express')
+const express = require('express')
+const { token } = require('morgan')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+    {
+        id: 1, 
+        name: "Arto Hellas", 
+        number: "040-123456"
+    }, 
+    {
+        id: 2, 
+        name: "Ada Lovelace", 
+        number: "39-44-5323523"
+    }, 
+    {
+        id: 3, 
+        name: "Dan Abramov", 
+        number: "12-43-234345"
+    }, 
+    {
+        id: 4, 
+        name: "Mary Poppendieck", 
+        number: "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+    const maxId = persons.length > 0 
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+    return maxId + 1
+}
+
+app.put('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const updatedNumber = request.body.number
+
+    let updatedContactList = persons.map(person => {
+        if(person.id === id) {
+            return {...person, number: updatedNumber}
+        } else {
+            return person
+        }
+    })
+    
+    persons = updatedContactList 
+
+    response.json(persons)
+})
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if(!body.name) {
+        console.log(body)
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    } 
+
+    let newPerson = {
+        id: generateId(),
+        name: body.name, 
+        number: body.number
+    }
+    
+    persons = persons.concat(newPerson)
+    response.json(persons) 
+    // console.log(persons)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+
+    response.json(person)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const people = persons.length
+    const timeStamp = new Date()
+    response.send(`<h4>Phonebook has info for ${people} people</h4>
+    <h5>${timeStamp}</h5>`)
+})
+
+
+const PORT = 3001 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

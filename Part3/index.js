d472bdf83ea6d68502b993d0c35f1e3a1ffca7e2
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-17 16:18:22 -0400

    remove temporary hardcode testing database

--- Part3/index.js
+++ Part3/index.js
@@ -11,28 +11,6 @@ app.use(cors())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        id: 1, 
-        name: "Arto Hellas", 
-        number: "040-123456"
-    }, 
-    {
-        id: 2, 
-        name: "Ada Lovelace", 
-        number: "39-44-5323523"
-    }, 
-    {
-        id: 3, 
-        name: "Dan Abramov", 
-        number: "12-43-234345"
-    }, 
-    {
-        id: 4, 
-        name: "Mary Poppendieck", 
-        number: "39-23-6423122"
-    }
-]
 
 const generateId = () => {
     const maxId = persons.length > 0 

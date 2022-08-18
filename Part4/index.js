99606b76e14247950ca873f4d2ea566e65922c97
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-18 18:54:31 -0400

    refactor

--- Part4/index.js
+++ Part4/index.js
@@ -1,36 +1,9 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
-const Blog = require('./models/blog')
-const { PORT } = require('./utils/config')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
 
-app.use(cors())
-app.use(express.json())
+const server = http.createServer(app)
 
-morgan.token('body', request => JSON.stringify(request.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })

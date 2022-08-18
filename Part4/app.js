9112d17dde638505110ca191052f52fe30218ecf
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-18 19:58:41 -0400

    refactor router function to seperate file

--- Part4/app.js
+++ Part4/app.js
@@ -4,29 +4,13 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blog')
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogRouter)
 
 morgan.token('body', request => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = app

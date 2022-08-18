5f727391cde8c4201d7df24c8b50d689a3b6e34d
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-18 19:57:21 -0400

    initial commit

---
+++ Part4/controllers/blog.js
@@ -0,0 +1,71 @@
+const blogRouter = require('express').Router()
+const { request } = require('../app')
+const blog = require('../models/blog')
+const Blog = require('../models/blog')
+
+// GET all from server
+blogRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+// GET specific item from server by ID
+blogRouter.get('/:id', (request, response, next) => {
+  Blog
+    .findById(request.params.id)
+    .then(blogPost => {
+      if (blogPost) {
+        response.json(blogPost)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+// DELETE item from server by ID
+blogRouter.delete('/:id', (request, response, next) => {
+  Blog
+    .findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// POST item to server
+blogRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+// PUT update item on server
+blogRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const updatedBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog
+    .findByIdAndUpdate(request.params.id, updatedBlog, { edited: true })
+    .then(updatedBlogPost => {
+      response.json(updatedBlogPost)
+    })
+    .catch(error => next(error))
+
+})
+
+module.exports = blogRouter

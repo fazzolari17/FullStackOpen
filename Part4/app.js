02a9fd7b9bac40e9c8e795cb99d415d261025852
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-18 20:36:11 -0400

    refactor to use custom middleware

--- Part4/app.js
+++ Part4/app.js
@@ -1,16 +1,31 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
 const cors = require('cors')
-const Blog = require('./models/blog')
 const blogRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('Connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogRouter)
 
-morgan.token('body', request => JSON.stringify(request.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app

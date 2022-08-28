c8eb73ebc4ab1f9af137eebbe3d0777b5ff43ca8
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-28 13:22:13 -0400

    remove console.log

--- Part4/utils/middleware.js
+++ Part4/utils/middleware.js
@@ -24,6 +24,7 @@ const userExtractor = async (request, response, next) => {
 }
 
 const tokenExtractor = (request, response, next) => {
+  console.log('REQUEST', request)
   const authorization = request.get('authorization')
 
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {

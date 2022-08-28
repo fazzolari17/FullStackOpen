6291dcc32b4f911c088c123e92179ccf423f51ae
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-28 15:45:09 -0400

    fix eslint indent and quote problems

--- Part5/bloglist-frontend/src/components/BlogForm.js
+++ Part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,22 +1,19 @@
-import React from "react"
-import blogService from "../services/blogService"
+import React from 'react'
+import blogService from '../services/blogService'
 
 function BlogForm({ user, setUser, setNewBlog, newBlog, setBlogs }) {
 
   const addBlog = (event) => {
-      event.preventDefault()
-      event.target.reset()
+    event.preventDefault()
+    event.target.reset()
 
-      blogService.create(newBlog)
-      setNewBlog({...newBlog, added: true })
-      
+    blogService.create(newBlog)
+    setNewBlog({...newBlog, added: true })
 
-      setTimeout(() => {
-        setNewBlog({ title: '', author: '', url: '', added: false })
-      }, 5000);
-      // blogService
-      //   .getAll(user.token)
-      //   .then(blogsFromServer => setBlogs( blogsFromServer ))
+    setTimeout(() => {
+      setNewBlog({ title: '', author: '', url: '', added: false })
+    }, 5000)
+
 
   }
 
@@ -28,34 +25,36 @@ function BlogForm({ user, setUser, setNewBlog, newBlog, setBlogs }) {
   }
 
   return (
+    
     <>
-    {user !== null && 
+      {user !== null && 
       <div className="userLoggedIn"><p>{user.name} is logged in</p>
-      <button className="logout-btn" onClick={logout}>Logout</button></div>}
+        <button className="logout-btn" onClick={logout}>Logout</button>
+      </div>}
 
-<h3>Add New Blog</h3>
-    <form onSubmit={addBlog}>
-      <label>
-        title
-        <input type='text' 
-        value={newBlog.title}
-        onChange={e => setNewBlog(prev => prev = { ...prev, title: e.target.value})}></input>
-      </label><br/>
-      <label>
-        author
-        <input type='text'
-        value={newBlog.author}
-        onChange={e => setNewBlog(prev => prev = {...prev, author: e.target.value})}></input>
-      </label><br />
-      <label>
-        url
-        <input type='text'
-        value={newBlog.url}
-        onChange={e => setNewBlog(prev => prev = {...prev, url: e.target.value})}></input>
-      </label><br />
-      <button type='submit'>Add Blog</button>
+      <h3>Add New Blog</h3>
+      <form onSubmit={addBlog}>
+        <label>
+          title
+          <input type='text' 
+            value={newBlog.title}
+            onChange={e => setNewBlog(prev => prev = { ...prev, title: e.target.value})}></input>
+        </label><br/>
+        <label>
+          author
+          <input type='text'
+            value={newBlog.author}
+            onChange={e => setNewBlog(prev => prev = {...prev, author: e.target.value})}></input>
+        </label><br />
+        <label>
+          url
+          <input type='text'
+            value={newBlog.url}
+            onChange={e => setNewBlog(prev => prev = {...prev, url: e.target.value})}></input>
+        </label><br />
+        <button type='submit'>Add Blog</button>
 
-    </form>
+      </form>
     </>
   )
 }

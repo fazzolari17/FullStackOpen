7b5d68780981ff6b26a4f507a18f1448d1133fc5
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-28 13:20:19 -0400

    initial commit

---
+++ Part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,63 @@
+import React from "react"
+import blogService from "../services/blogService"
+
+function BlogForm({ user, setUser, setNewBlog, newBlog, setBlogs }) {
+
+  const addBlog = (event) => {
+      event.preventDefault()
+      event.target.reset()
+
+      blogService.create(newBlog)
+      setNewBlog({...newBlog, added: true })
+      
+
+      setTimeout(() => {
+        setNewBlog({ title: '', author: '', url: '', added: false })
+      }, 5000);
+      // blogService
+      //   .getAll(user.token)
+      //   .then(blogsFromServer => setBlogs( blogsFromServer ))
+
+  }
+
+  const logout = () => {
+    localStorage.removeItem('loggedInUser')
+    setUser(null)
+
+    setBlogs([])
+  }
+
+  return (
+    <>
+    {user !== null && 
+      <div className="userLoggedIn"><p>{user.name} is logged in</p>
+      <button className="logout-btn" onClick={logout}>Logout</button></div>}
+
+<h3>Add New Blog</h3>
+    <form onSubmit={addBlog}>
+      <label>
+        title
+        <input type='text' 
+        value={newBlog.title}
+        onChange={e => setNewBlog(prev => prev = { ...prev, title: e.target.value})}></input>
+      </label><br/>
+      <label>
+        author
+        <input type='text'
+        value={newBlog.author}
+        onChange={e => setNewBlog(prev => prev = {...prev, author: e.target.value})}></input>
+      </label><br />
+      <label>
+        url
+        <input type='text'
+        value={newBlog.url}
+        onChange={e => setNewBlog(prev => prev = {...prev, url: e.target.value})}></input>
+      </label><br />
+      <button type='submit'>Add Blog</button>
+
+    </form>
+    </>
+  )
+}
+
+export default BlogForm

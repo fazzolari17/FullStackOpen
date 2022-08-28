0990f5ff6d8e382607be2319afef7b1e1ec0b910
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-28 13:12:48 -0400

    initial commit

---
+++ Part5/bloglist-frontend/src/components/ErrorMessage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function ErrorMessage({ message }) {
+  return (
+    <div className="errorMsg">
+    <h2>{message}</h2>
+    </div>
+  )
+}
+
+export default ErrorMessage

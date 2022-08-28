32d1bf88c22b561366efe601d3b0571e435eae97
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-28 13:19:32 -0400

    initial commit

---
+++ Part5/bloglist-frontend/src/services/blogService.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+console.log(token)
+
+const getAll = (token) => {
+  const config = {
+    headers: { 
+      'Authorization': `bearer ${token}` }
+  }
+  const request = axios.get(baseUrl, config)
+
+  return request.then(response => response.data)
+}
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, update, setToken }

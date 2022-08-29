3e560571099422174c0e0e1091881f8eacda4cfe
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-29 13:26:51 -0400

    add ux and ux functionality

--- Part5/bloglist-frontend/src/components/Blog.js
+++ Part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,71 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+import blogService from '../services/blogService'
+
+const Blog = ({ blog, user, setBlogs }) => {
+  const [ visible, setVisible ] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const addLike = async () => {
+    blogService.update(blog.id, {
+      ...blog,
+      likes: parseInt(blog.likes) + 1
+    })
+    const fromDb = await blogService
+      .getAll(user.token)
+
+    setBlogs(fromDb)
+  }
+
+  const removeBlog = async () => {
+    if(window.confirm(`Remove ${blog.title} by ${blog.author}`) === false) {
+      return
+    }
+
+    await blogService.remove(blog.id)
+    const blogsFromDb = await blogService.getAll(user.token)
+    setBlogs(blogsFromDb)
+  }
+
+  return (
+
+    <div>
+      <div style={Object.assign(hideWhenVisible, blogStyle)}>
+        {blog.title} {blog.author}
+
+        <div>
+          <button onClick={() => setVisible(true)}>Show</button>
+        </div>
+      </div>
+
+      <div style={Object.assign(showWhenVisible, blogStyle)}>
+        {blog.title}
+        {blog.author} <br/>
+        <a href={`https://${blog.url}`} target='blank'>{blog.url}</a><br />
+        Likes: {blog.likes}
+        <button className='likeBtn' onClick={addLike} >like</button>
+        <br/>
+        {blog.user.name}
+
+        <div>
+          <button onClick={() => setVisible(false)}>Hide</button>
+        </div>
+
+        <div>
+          <button onClick={removeBlog}>remove</button>
+        </div>
+      </div>
+
+    </div>
+  )}
 
 export default Blog

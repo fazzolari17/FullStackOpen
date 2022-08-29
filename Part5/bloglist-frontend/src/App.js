bb433c6532e6601ad030b2948af4b22a90816533
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-29 13:25:45 -0400

    rerfactor code and add ux improvements

--- Part5/bloglist-frontend/src/App.js
+++ Part5/bloglist-frontend/src/App.js
@@ -11,7 +11,9 @@ const App = () => {
   const [ blogs, setBlogs ] = useState([])
   const [ user, setUser ] = useState(null)
   const [ errorMessage, setErrorMessage ] = useState(null)
-  const [ newBlog, setNewBlog ] = useState({title: '', author: '', url: '', added: false})
+  const [ newBlog, setNewBlog ] = useState({ title: '', author: '', url: '', added: false })
+
+
 
   useEffect(() => {
     const loggedInUserJSON = localStorage.getItem('loggedInUser')
@@ -19,41 +21,45 @@ const App = () => {
     if (loggedInUserJSON) {
       const user = JSON.parse(loggedInUserJSON)
       setUser(user)
+
       blogService.setToken(user.token)
-      console.log(user.token)
-      
+
       blogService
         .getAll(user.token)
         .then(blogsFromServer => setBlogs( blogsFromServer ))
     }
-  }, [newBlog])
-  
-  const blogsMapped = blogs.map(blog =>
-    <Blog key={blog.id} blog={blog} />
+  }, [ newBlog ])
+
+  const blogsMappedFromServer = blogs.map(blog =>
+    <Blog key={blog.id} blog={blog} user={user} setBlogs={setBlogs}/>
   )
 
-  return ( 
+  const sortedByLikes = blogsMappedFromServer.sort((a, b) => parseInt(b.props.blog.likes) - parseInt(a.props.blog.likes))
+
+  return (
     <div>
       <h2>Blogs</h2>
       {errorMessage !== null && <ErrorMessage message={errorMessage}/>}
       {newBlog.added && <SuccessMessage newBlog={newBlog}/>}
-      {user === null &&
-        <LoginForm 
+      {user === null ?
+        <LoginForm
           setUser={setUser}
           user={user}
           setErrorMessage={setErrorMessage}
           errorMessage={errorMessage}
           setBlogs={setBlogs}
-        /> }
-      <BlogForm 
-        user={user}
-        setUser={setUser}
-        setNewBlog={setNewBlog}
-        newBlog={newBlog}
-        setBlogs={setBlogs}
-      />
+        />
 
-      { blogsMapped }
+        :
+        <BlogForm
+          user={user}
+          setUser={setUser}
+          setNewBlog={setNewBlog}
+          newBlog={newBlog}
+          setBlogs={setBlogs}
+        />}
+
+      { sortedByLikes }
     </div>
   )
 }

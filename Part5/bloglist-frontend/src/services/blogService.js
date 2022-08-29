c0f93277591b9a127e2db3e382be930d415a14af
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-29 13:29:17 -0400

    refactor all fn to async / await

--- Part5/bloglist-frontend/src/services/blogService.js
+++ Part5/bloglist-frontend/src/services/blogService.js
@@ -1,34 +1,39 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-let token = null
+let globalToken = null
 
-const setToken = newToken => {
-  token = `bearer ${newToken}`
+const setToken = async (newToken) => {
+  globalToken = `bearer ${newToken}`
 }
 
-const getAll = (token) => {
+const getAll = async (token) => {
   const config = {
-    headers: { 
+    headers: {
       'Authorization': `bearer ${token}` }
   }
-  const request = axios.get(baseUrl, config)
-
-  return request.then(response => response.data)
+  const request = await axios.get(baseUrl, config)
+  return request.data
 }
 
 const create = async newObject => {
   const config = {
-    headers: { Authorization: token }
+    headers: { Authorization: globalToken }
   }
 
   const response = await axios.post(baseUrl, newObject, config)
   return response.data
 }
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
+const update = async (id, newObject) => {
+  const request = await axios.put(`${ baseUrl }/${ id }`, newObject, { headers: { 'Authorization': globalToken } })
+  return request.data
 }
 
-export default { getAll, create, update, setToken }
+const remove = async (id) => {
+  const request = await axios.delete(`${ baseUrl }/${ id }`, { headers: { 'Authorization': globalToken } })
+  return request
+}
+
+
+export default { getAll, create, update, setToken, remove }

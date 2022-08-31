8006b51d469b75316893ad4ebb217619c347f2f3
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-31 16:58:37 -0400

    remove console log

--- Part5/bloglist-frontend/src/components/SuccessMessage.js
+++ Part5/bloglist-frontend/src/components/SuccessMessage.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 function SuccessMessage({ newBlog }) {
   const { title, author } = newBlog
-  console.log(title, author)
   return (
     <div className="successMsg">
       <h3>A new blog {title} by {author} added</h3>

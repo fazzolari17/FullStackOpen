9dfda6c232a5ab4ac22e7fa6cfff647fc81d31e2
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-31 17:05:33 -0400

    add aria-labels for testing and accessability

--- Part5/bloglist-frontend/src/components/BlogForm.js
+++ Part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,28 +1,29 @@
 import React from 'react'
 
-function BlogForm({ setNewBlog,  addBlog }) {
+function BlogForm({ setNewBlog, handleSubmit }) {
 
   return (
 
     <div>
-      <h3>Add New Blog</h3>
-      <form onSubmit={addBlog}>
+      <h3 aria-label='add new blog' >Add New Blog</h3>
+      <form onSubmit={handleSubmit}>
         <label>
           title
-          <input type='text'
-            onChange={e => setNewBlog(prev => prev = { ...prev, title: e.target.value })}></input>
+          <input type='text' aria-label='blog title'
+            onChange={e => setNewBlog(prev => prev = { ...prev, title: e.target.value })}>
+          </input>
         </label><br/>
         <label>
           author
-          <input type='text'
+          <input type='text' aria-label='blog author'
             onChange={e => setNewBlog(prev => prev = { ...prev, author: e.target.value })}></input>
         </label><br />
         <label>
           url
-          <input type='text'
+          <input type='text' aria-label='blog url'
             onChange={e => setNewBlog(prev => prev = { ...prev, url: e.target.value })}></input>
         </label><br />
-        <button type='submit'>Add Blog</button>
+        <button role='submit' aria-label='submit button' type='submit'>Add Blog</button>
 
       </form>
     </div>

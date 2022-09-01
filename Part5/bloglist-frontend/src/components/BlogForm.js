f4112ae228689ec208206ecdde43c7314ce080d1
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-09-01 16:46:41 -0400

    add aria labels and data attributes for better test integration

--- Part5/bloglist-frontend/src/components/BlogForm.js
+++ Part5/bloglist-frontend/src/components/BlogForm.js
@@ -9,21 +9,26 @@ function BlogForm({ setNewBlog, handleSubmit }) {
       <form onSubmit={handleSubmit}>
         <label>
           title
-          <input type='text' aria-label='blog title'
+          <input type='text' aria-label='blog title' data-cy='title_input'
             onChange={e => setNewBlog(prev => prev = { ...prev, title: e.target.value })}>
           </input>
         </label><br/>
         <label>
           author
-          <input type='text' aria-label='blog author'
+          <input type='text' aria-label='blog author' data-cy='author_input'
             onChange={e => setNewBlog(prev => prev = { ...prev, author: e.target.value })}></input>
         </label><br />
         <label>
           url
-          <input type='text' aria-label='blog url'
+          <input type='text' aria-label='blog url' data-cy='blog_url_input'
             onChange={e => setNewBlog(prev => prev = { ...prev, url: e.target.value })}></input>
         </label><br />
-        <button role='submit' aria-label='submit button' type='submit'>Add Blog</button>
+        <button
+          role='submit'
+          aria-label='submit button'
+          type='submit'
+          data-cy='add_blog_btn'
+        >Add Blog</button>
 
       </form>
     </div>

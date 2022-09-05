174f0310ab3fa06e71f888e08218c06903a9823e
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-09-05 11:37:45 -0400

    refactor to use notificationSlice

--- Part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ Part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,61 +1,21 @@
+import { createAction, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   message: 'Reverted to previous commit to fix this',
   isShowing: false
 }
-
-const NotificationReducer = (state = initialState, action) => {
-  switch (action.type) {
-  case 'CHANGE_MESSAGE':
-    return { ...state, message: action.payload }
-  case 'CHANGE_SHOW':
-    return { ...state, isShowing: !state.isShowing }
-  default:
-    return state
-
+const notificationSlice = createSlice({
+  name:'message',
+  initialState,
+  reducers: {
+    changeMessage(state, action) {
+      return { ...state, message: action.payload }
+    },
+    messageVisibility(state, action) {
+      return { ...state, isShowing: !state.isShowing }
+    }
   }
-}
+})
 
-export const changeMessage = newMessage => {
-  return {
-    type: 'CHANGE_MESSAGE',
-    payload: newMessage
-  }
-}
-
-export const messageVisibility = () => {
-  return {
-    type: 'CHANGE_SHOW'
-
-  }
-}
-
-
-// export const { changeMessage, messageVisibility }
-
-export default NotificationReducer
-
-// import { createSlice } from '@reduxjs/toolkit'
-
-// const initialState = {
-//   message: 'Inside State Finally',
-//   isShowing: false
-// }
-
-// const options = {
-//   name: 'notification',
-//   initialState,
-//   reducers: {
-//     displayNotification(state, action) {
-//       return { ...state, isShowing: !state.isShowing }
-//     },
-//     changeNotification(state, action) {
-//       return { ...state, message: action.payload }
-//     }
-//   }
-// }
-
-// const notificationMessageSlice = createSlice(options)
-
-// export const { displayNotification, changeNotification } = notificationMessageSlice.actions
-// export default notificationMessageSlice
+export const { changeMessage, messageVisibility } = notificationSlice.actions
+export default notificationSlice.reducer

22decfcf7fa83a1683d6f9507510c9f1b2fd0a6f
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-09-05 11:08:40 -0400

    refactor to remove combine reducers

--- Part6/redux-anecdotes/src/store.js
+++ Part6/redux-anecdotes/src/store.js
@@ -1,17 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { combineReducers } from '@reduxjs/toolkit'
 import addAnecdote from './reducers/anecdoteReducer'
 import addVote from './reducers/anecdoteReducer'
 import notificationMessage from './reducers/notificationReducer'
 
-const reducers = combineReducers({
-  anecdotes: addAnecdote,
-  votes: addVote,
-  message: notificationMessage
 
-
+const store = configureStore({
+  reducer: {
+    anecdotes: addAnecdote,
+    votes: addVote,
+    message: notificationMessage
+  }
 })
-const store = configureStore({ reducer: { reducers } })
 
 
 export default store

b8a0edf32980443bf833efae0e706b0228d43f09
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-09-07 20:11:41 -0400

    exercises 7.4 - 7.8 initial commit

--- Part7/routed-anecdotes/src/components/CreateNew.js
+++ Part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,25 +1,32 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = ({ addNew, setNotification }) => {
-  // const [ notificationMessage, setNotificationMessage ] = useState()
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
   const navigate = useNavigate()
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    handleNotification(content)
+    handleNotification(content.value)
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate(`/`)
   }
+
+  const handleReset = () => {
+    content.onReset()
+    author.onReset()
+    info.onReset()
+  }
+
 
   const handleNotification = content => {
     setNotification({ content: content, isVisible: true })
@@ -35,17 +42,18 @@ const CreateNew = ({ addNew, setNotification }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type="reset" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
---
+++ Part7/ultimate-hooks/src/App.js
@@ -0,0 +1,94 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+  const onReset = () => {
+    setValue('')
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    onReset
+  }
+}
+
+const useResource = (baseUrl, content) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    axios.get(baseUrl).then(r => setResources( prev => r.data )) 
+  }, [ baseUrl, setResources ])
+  
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    return response.data
+  }
+  
+  const create = async (content) => {
+    const newObject = content 
+    
+    const response = await axios.post(baseUrl, newObject)
+    const getResponse = await axios.get(baseUrl)
+    setResources(getResponse.data)
+    return response.data
+  }
+  
+  const service = {
+    getAll,
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
+
+const App = () => {
+  const content = useField('text')
+  const name = useField('text')
+  const number = useField('text')
+
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
+    content.onReset()
+  }
+ 
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value})
+    name.onReset()
+    number.onReset()
+  }
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
+}
+
+export default App

0a79e4d38618f303774158051dc4ad03bd70f571
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-09-08 11:53:43 -0400

    initial commit

---
+++ Part5/bloglist-frontend/src/reducers/notificationReducer.js
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const initialState = {
+  message: '',
+  isVisible: false,
+  className: ''
+}
+
+const notificationSlice = createSlice({
+  name:'message',
+  initialState,
+  reducers: {
+    changeMessage(state, action) {
+      return { ...state, message: action.payload }
+    },
+    messageVisibility(state, action) {
+      return { ...state, isVisible: action.payload }
+    },
+    setClass(state, action) {
+      return { ...state, className: action.payload }
+    }
+  }
+})
+
+let timeoutId
+
+export const setNotification = (message, className, displayTime) => {
+  return async dispatch => {
+    dispatch(changeMessage(message))
+    dispatch(messageVisibility(true))
+    dispatch(setClass(className))
+
+    if(typeof timeoutId === 'number') {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => {
+        dispatch(messageVisibility(false))
+      }, displayTime)
+    }
+
+    timeoutId = setTimeout(() => {
+      dispatch(messageVisibility(false))
+    }, displayTime)
+
+  }
+
+
+}
+export const { changeMessage, messageVisibility, setClass } = notificationSlice.actions
+export default notificationSlice.reducer

d0d224ac4a6822d6d21a42ea7b2412cdb3738352
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-09-10 09:28:28 -0400

    add classname to facilitate styling

--- Part5/bloglist-frontend/src/components/BlogForm.js
+++ Part5/bloglist-frontend/src/components/BlogForm.js
@@ -7,27 +7,26 @@ function BlogForm({ handleSubmit }) {
 
   return (
 
-    <div>
-      <h3 aria-label='add new blog' >Add New Blog</h3>
+    <div className='blog_form'>
+      <h3 className='blog_form_title' aria-label='add new blog' >Add New Blog</h3>
       <form onSubmit={handleSubmit}>
-        <label>
+        <label className='label'>
           title
-          <input type='text' aria-label='blog title' data-cy='title_input'
+          <input className='form_fields' type='text' aria-label='blog title' data-cy='title_input'
             onChange={e => dispatch(addTitle(e.target.value))}>
           </input>
         </label><br/>
-        <label>
+        <label className='form_fields_container'>
           author
-          <input type='text' aria-label='blog author' data-cy='author_input'
+          <input className='form_fields' type='text' aria-label='blog author' data-cy='author_input'
             onChange={e => dispatch(addAuthor(e.target.value))}></input>
         </label><br />
         <label>
           url
-          <input type='text' aria-label='blog url' data-cy='blog_url_input'
+          <input className='form_fields' type='text' aria-label='blog url' data-cy='blog_url_input'
             onChange={e => dispatch(addUrl(e.target.value))}></input>
         </label><br />
         <button
-          role='submit'
           aria-label='submit button'
           type='submit'
           data-cy='add_blog_btn'

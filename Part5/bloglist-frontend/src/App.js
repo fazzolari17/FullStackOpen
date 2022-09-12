784eade5e4a42a6cea937b3defb4bc2dfe76ff8b
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-09-12 13:29:18 -0400

    exercises 7.16 - 7.19

--- Part5/bloglist-frontend/src/App.js
+++ Part5/bloglist-frontend/src/App.js
@@ -1,15 +1,14 @@
 import './style.css'
 import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { BrowserRouter as Router, useParams } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 
-import Blog from './components/Blog'
+import blogService from './services/blogService'
 import BlogList from './components/BlogList'
 import SingleBlog from './components/SingleBlog'
 import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
-import blogService from './services/blogService'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
 import Users from './components/Users'
@@ -18,7 +17,7 @@ import { setNotification } from './reducers/notificationReducer'
 import { toggleAdded } from './reducers/blogFormReducer'
 import { resetLoginForm } from './reducers/loginFormReducer'
 import { login, resetUserInfo, setUser } from './reducers/userReducer'
-import { initializeState, removeAllBlogs, createNew, removeBlogFromState, addLikes, } from './reducers/blogPostListReducer'
+import { initializeState, removeAllBlogs, createNew, removeBlogFromState, addLikes, addComments } from './reducers/blogPostListReducer'
 import { fetchUsers, resetAllUsers, setAllUsers } from './reducers/allUsers'
 
 const App = () => {
@@ -27,7 +26,6 @@ const App = () => {
   const blogForm = useSelector(state => state.blogForm)
   const loginForm = useSelector(state => state.loginForm)
   const user = useSelector(state => state.user)
-
   const blogFormRef = useRef()
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -36,18 +34,17 @@ const App = () => {
     //Get Authentication token from local storage
     const allUsersJSON = localStorage.getItem('allUsers')
     const loggedInUserJSON = localStorage.getItem('loggedInUser')
-
     if (loggedInUserJSON) {
       const user = JSON.parse(loggedInUserJSON)
       const allUsers = JSON.parse(allUsersJSON)
+      console.log(user.token)
+      blogService.setToken(user.token)
 
       dispatch(setUser(user))
       dispatch(setAllUsers(allUsers))
-
-      blogService.setToken(user.token)
       dispatch(initializeState(user.token))
     }
-  }, [blogForm.added])
+  }, [])
 
   const logout = () => {
     localStorage.removeItem('loggedInUser')
@@ -61,21 +58,19 @@ const App = () => {
   const handleLogin = async (event) => {
     event.preventDefault()
     event.target.reset()
-
     dispatch(login(loginForm.username, loginForm.password))
     dispatch(resetLoginForm())
     dispatch(fetchUsers())
     navigate('/users')
+
   }
 
   const addBlog = e => {
     e.preventDefault()
     e.target.reset()
-
     dispatch(setNotification(`A new blog ${blogForm.title} by ${blogForm.author} added`, 'successMsg', '5000'))
     blogFormRef.current.toggleVisibility()
     dispatch(createNew({ ...blogForm, user: user.name }))
-
     dispatch(toggleAdded('2000'))
   }
 
@@ -86,7 +81,6 @@ const App = () => {
       return
     }
     dispatch(removeBlogFromState(blog.id, user.token))
-    dispatch(initializeState(user.token))
     navigate('/blogs')
 
   }
@@ -102,8 +96,13 @@ const App = () => {
     dispatch(initializeState(user.token))
   }
 
+  const addComment = newComment => {
+    dispatch(addComments(user.token, newComment))
+  }
+
   const padding = {
-    paddingLeft: '.5rem'
+    paddingLeft: '.5rem',
+    color: '#F7F0F0'
   }
 
   return (
@@ -114,12 +113,11 @@ const App = () => {
         <Link style={padding} to='/users'>Users</Link>
         <Link style={padding} to='/login'>Login</Link>
         {user !== null && <div className="userLoggedIn"><p>{user.name} is logged in</p>
-          <button data-cy='logout_btn' onClick={logout}>Logout</button>
+          <button data-cy='logout_btn' id='logout_btn' onClick={logout}>Logout</button>
         </div>}
       </nav>
       {notification.isVisible && <Notification />}
       <Routes>
-        {/* ========================================================================================= */}
 
         <Route path='/' element={<h1>Home</h1>}/>
 
@@ -127,7 +125,7 @@ const App = () => {
           <Togglable classname={'loginToggle'} visible={true} buttonId={'login_cancel_btn'} buttonLabel={'Login'}>
             <LoginForm handleLogin={handleLogin} />
           </Togglable>} />
-        {/* ========================================================================================= */}
+
         <Route
           path='/users/:id'
           element={
@@ -140,17 +138,16 @@ const App = () => {
         <Route path='/users' element={<Users />} />
 
         <Route path ='/blogs/:id' element={
-          <SingleBlog  handleLike={addLike} handleRemove={removeBlog} />
+          <SingleBlog  handleLike={addLike} handleRemove={removeBlog} handleComment={addComment}/>
         } />
 
         <Route path='/blogs' element={
           <div>
-            <h2>Blogs</h2>
+            <h2 style={{ margin: '1rem' }} >Blogs</h2>
             <Togglable visible={false} buttonId={'new_blog_button'} buttonLabel={'New Blog'} ref={blogFormRef}>
               <BlogForm handleSubmit={addBlog} />
             </Togglable>
             <BlogList addLike={addLike} removeBlog={removeBlog} />
-            {/* {sortedByLikes} */}
           </div>} />
 
       </Routes>

72832f8482596e5988a4fa6d0521b598889a0620
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-09-12 15:43:51 -0400

    add functionality for comments and styles

--- Part5/bloglist-frontend/src/components/SingleBlog.js
+++ Part5/bloglist-frontend/src/components/SingleBlog.js
@@ -1,7 +1,9 @@
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import CommentForm from './CommentForm'
 
-const SingleBlog = ({ handleLike, handleRemove }) => {
+
+const SingleBlog = ({ handleLike, handleRemove, handleComment }) => {
   const blogs = useSelector(state => state.blogs)
   const blogId = useParams()
   const user = useSelector(state => state.user)
@@ -11,17 +13,28 @@ const SingleBlog = ({ handleLike, handleRemove }) => {
   }
 
   const blog = blogs.find(blog => blog.id === blogId.id)
+  const comments = blog.comments.map(comment =>
+    <li  key={comment.id}>{comment.comment}</li>)
 
   return (
     <div className='sb_main_container'>
       <h2 className='sb_title'>{`${blog.title} ${blog.author}`}</h2>
-      <a className='sb_url' href={`https://${blog.url}`} target='blank'>{`https://${blog.url}`}</a>
+
       <div className='sb_likes_container'>
-        <p>{blog.likes} Likes</p><button onClick={e => handleLike(blog.id)}>Like</button>
+        <p>{blog.likes} Likes</p><button className='like_btn' onClick={e => handleLike(blog.id)}>Like</button>
       </div>
-      <p>Added By: {blog.user.username}</p>
+
+      <a className='sb_url' href={`https://${blog.url}`} target='blank'>{`https://${blog.url}`}</a>
+      <p className='sb_single_blog_user'>Added By: {blog.user.username}</p>
+
       {/* Conditionally render Remove Btn  */}
-      {user.name === blog.user.name && <button onClick={e => handleRemove(blog.id)}>Remove</button>}
+      {user.id === blog.user.id && <button className='remove_btn' onClick={e => handleRemove(blog.id)}>Remove</button>}
+
+      <h4 style={{ margin: '.5rem 0' }}>Comments</h4>
+      <CommentForm handleComment={handleComment}/>
+      <ul className='comments'>
+        {comments}
+      </ul>
     </div>
   )
 }
